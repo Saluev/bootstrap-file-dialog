2aafd661baa6470d62f87fd6b0cbe9ea4f13ac2b
Tigran Saluev <Saluev@users.noreply.github.com>
2014-07-17 01:12:37 +0400

    Fixing backdrop problem using hint by @cvrebert (https://github.com/twbs/bootstrap/issues/12990#issuecomment-49217496)

--- bootstrap.fd.js
+++ bootstrap.fd.js
@@ -27,9 +27,9 @@
         <button type="button" class="btn btn-primary bfd-ok">{{ok_button}}</button>\
         <button type="button" class="btn btn-default bfd-cancel" data-dismiss="modal">{{cancel_button}}</button>\
       </div>\
-    </div><!-- /.modal-content -->\
-  </div><!-- /.modal-dialog -->\
-</div><!-- /.modal -->';
+    </div>\
+  </div>\
+</div>';
         modal_html = modal_html.replace(/{{title}}/g, options.title);
         modal_html = modal_html.replace(/{{ok_button}}/g, options.ok_button);
         modal_html = modal_html.replace(/{{cancel_button}}/g, options.cancel_button);
